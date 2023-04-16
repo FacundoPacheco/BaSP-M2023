915df6020ad8eb41046266e8317cc916f5fe1ef9
Facundo Pacheco <pachecofaction7@gmail.com>
2023-04-15 21:52:56 -0300

    BaSP-W05-2023 02 strings finished

--- Week-05/excercises/02-strings.js
+++ Week-05/excercises/02-strings.js
@@ -10,3 +10,27 @@ var favouriteFood = "papasfritas";
 var favouriteFoodB = favouriteFood.substring(8,11);
 console.log(favouriteFoodB);
 
+var activity = "diagnosticar";
+var activityB = activity.substring(0,1).toUpperCase();
+var activityC = activity.substring(1,activity.length).toLowerCase();
+var activityDefinite = activityB + activityC;
+console.log(activityDefinite);
+
+var food = "spaghettis ";
+var foodfast = food.indexOf(" ");
+console.log(foodfast);
+
+var movie = "tiempos violentos";
+var movieMayuscOne = movie.substring(0,1).toUpperCase();
+var movieMayuscOneB = movieMayuscOne + movie.substring(1,movie.indexOf(" ")).toLowerCase()
+var movieMayuscTwo = movie.substring(movie.indexOf(" ") + 1, movie.indexOf(" ") + 2).toUpperCase();
+console.log(movieMayuscTwo)
+var movieMayuscTwoB = movieMayuscTwo + movie.substring(movie.indexOf(" ") + 2).toLowerCase()
+var moviedefinitive = movieMayuscOneB + " " + movieMayuscTwoB;
+console.log(moviedefinitive);
+
+
+
+
+
+
