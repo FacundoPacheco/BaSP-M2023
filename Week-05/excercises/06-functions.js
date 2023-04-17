50e608f9affdf82864845a21f56bf558bf6478e0
Facundo Pacheco <pachecofaction7@gmail.com>
2023-04-17 16:28:07 -0300

    BaSP-W05-2023 06-a) finished

---
+++ Week-05/excercises/06-functions.js
@@ -0,0 +1,7 @@
+//Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.
+function suma(a,b) {
+    return a + b;
+}
+var resultado = suma(4,7);
+console.log(resultado);
+
