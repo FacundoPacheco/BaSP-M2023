142a771c126a16b7cb6388f5d6d127422d87be38
Facundo Pacheco <pachecofaction7@gmail.com>
2023-04-17 15:43:53 -0300

    BaSP-W05-2023 05-for.js not comments alerts

--- Week-05/excercises/05-for.js
+++ Week-05/excercises/05-for.js
@@ -1,13 +1,13 @@
 // Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras.
 var service = new Array("internet", "teléfono", "netflix", "luz", "agua");
 for (let index = 0; index < service.length; index++) {
-    //alert(service[index]);
+    alert(service[index]);
 }
 
 //Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.
 for (let index = 0; index < service.length; index++) {
     var serviceMayusc = service[index].substring(0,1).toUpperCase() + service[index].substring(1,service[index].length)
-    //alert("Se modificó: " + serviceMayusc);
+    alert("Se modificó: " + serviceMayusc);
 }
 
 //Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa.
